3b3be6a82ee652902343bb6d6b5e618352d59f63
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-20 18:15:37 +0300

    нашли расстояние между точками

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите координаты X1: ");
+int x1 = int.Parse (Console.ReadLine ());
+
+Console.Write ("Введите координаты Y1: ");
+int y1 = int.Parse (Console.ReadLine ());
+
+Console.Write ("Введите координаты X2: ");
+int x2 = int.Parse (Console.ReadLine ());
+
+Console.Write ("Введите координаты Y2: ");
+int y2 = int.Parse (Console.ReadLine ());
+
+ 
+
+double dist = Math.Sqrt (Math.Pow (x2 - x1, 2) + Math.Pow (y2 -y1, 2));
+Console.WriteLine ($"Расстояние между точками {dist:f2}");
