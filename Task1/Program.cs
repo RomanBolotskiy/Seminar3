724414514bbf9d13abc46f7954ec467bd5502682
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-20 16:54:33 +0300

    Вычислили в какой четверти находится точка на системе координат ХУ

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write ("Введите X: ");
+int x = int.Parse (Console.ReadLine ());
+Console.Write ("Введите Y: ");
+int y = int.Parse (Console.ReadLine ());
+
+if (x > 0 && y > 0)
+{
+    Console.Write ("1");
+}
+
+if (x < 0 && y > 0)
+{
+    Console.Write ("2");
+}
+
+if (x < 0 && y < 0)
+{
+    Console.Write ("3");
+}
+
+if (x > 0 && y < 0)
+{
+    Console.Write ("4");
+}
