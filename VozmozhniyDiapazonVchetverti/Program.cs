20856c029413d8c283ad1b4a808c47faf76c69a0
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-20 17:45:12 +0300

    Находим варианты координат по заданной четверти

---
+++ VozmozhniyDiapazonVchetverti/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите номер четверти от 1 до 4: ");
+int num = int.Parse (Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+if (num == 2)
+{
+    Console.WriteLine("x < 0 и y > 0");
+}
+if (num == 3)
+{
+    Console.WriteLine("x < 0 и y < 0");
+}
+if (num == 4)
+{
+    Console.WriteLine("x > 0 и y < 0");
+}
