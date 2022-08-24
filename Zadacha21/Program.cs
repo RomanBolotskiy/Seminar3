6343f669a3d205f68d4d229ca7e95496dbb043e0
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-24 21:07:41 +0300

    Палиндром

--- Zadacha21/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine ("Введите число: ");
-int N = int.Parse (Console.ReadLine());
-
-for (int i = 1; i <= N; i++)
-{
-    Console.Write (Math.Pow (i, 2) + ", ");
-}
---
+++ Zadacha22/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.WriteLine ("Введите число: ");
+int N = int.Parse (Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write (Math.Pow (i, 2) + ", ");
+}
